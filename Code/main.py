903f0455bd88d913b15b1cae6a022d4f4db3255e
Lucasw54 <100497443+Lucasw54@users.noreply.github.com>
2023-06-11 17:45:40 -0400

    Adapted to Time Perception CSV + graphing

--- Code/main.py
+++ Code/main.py
@@ -1,25 +1,29 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import plotly.express as px
 
-dataset = pd.read_csv('cancer.csv')
+
+dataset = pd.read_csv('TimePerceptionV6.csv')
 
 x = dataset.drop(columns=["diagnosis(1=m, 0=b)"])
 y = dataset["diagnosis(1=m, 0=b)"]
 
-from sklearn.model_selection import train_test_split
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-import tensorflow as tf
-
-model=tf.keras.models.Sequential()
+model = tf.keras.models.Sequential()
 
 model.add(tf.keras.layers.Dense(256, input_shape=x_train.shape[1:], activation='sigmoid'))
-model.add(tf.keras.layers.Dense(256, activation='sigmoid'))
+model.add(tf.keras.layers.Dense(128, activation='sigmoid'))
+model.add(tf.keras.layers.Dense(64, activation='sigmoid'))
+model.add(tf.keras.layers.Dense(12, activation='relu'))
 model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 
-model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-history = model.fit(x_train, y_train, epochs = 1000)
+history = model.fit(x_train, y_train, epochs=1000)
 
 model.evaluate(x_test, y_test)
 
@@ -28,13 +32,15 @@ history.history.keys()
 z = 'accuracy'
 print(z)
 
+#plt.plot(history.history['accuracy'])
+#plt.plot(history.history['loss'])
+#plt.title('model accuracy/loss')
+#plt.ylabel('loss | accuracy')
+#plt.xlabel('epoch')
+#plt.legend(['accuracy', 'loss'], loc='upper left')
+#plt.show()
 
-import matplotlib.pyplot as plt
+df = pd.read_csv('TimePerceptionV6.csv')
 
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['loss'])
-plt.title('model accuracy/loss')
-plt.ylabel('loss | accuracy')
-plt.xlabel('epoch')
-plt.legend(['accuracy','loss'], loc = 'upper left')
-plt.show()
+fig = px.bar(df, x='Sleep (h)', y='Delta', title='Time Perception Madness')
+fig.show()
