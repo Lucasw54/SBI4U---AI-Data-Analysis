3d76572f2301de92ea4f4ae1e3f617a4b421a2ce
Lucasw54 <100497443+Lucasw54@users.noreply.github.com>
2023-06-12 16:25:09 -0400

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -1,3 +1,5 @@
+import matplotlib
+import matplotlip as matplotlip
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -5,6 +7,8 @@ from sklearn.model_selection import train_test_split
 import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 dataset = pd.read_csv('TimePerceptionV6.csv')
@@ -47,7 +51,7 @@ df = pd.read_csv('TimePerceptionV6.csv')
 fig = px.bar(df, x='Sleep (h)', y='Delta', title='Time Perception Madness')
 #fig.show()
 
-fig2 = px.bar(df, x='Sleep (h)', y='Delta', color='Time of Day (%)',title='Time Perception Madness')
+fig2 = px.bar(df, x='Sleep (h)', y='Delta', color='Age',title='Time Perception Madness')
 fig2.show()
 
 df = pd.read_csv('TimePerceptionV6.csv', delimiter=',', encoding='utf-8')
@@ -61,3 +65,43 @@ fig3.add_trace(go.Scatter(x=df['Sleep (h)'], y=line_of_best_fit, line=dict(color
 fig3.update_layout(title='Time Perception Madness')
 fig3.update_layout(xaxis_title='Sleep (h)', yaxis_title='Delta')
 fig3.show()
+
+#G = nx.Graph()
+
+#Add nodes
+#G.add_node('Berea College')
+#G.add_nodes_from(range(1,10))
+
+#Remove nodes
+#G.remove_node('Berea College')
+#G.remove_nodes_from([(1,2), (2,3)])
+
+#adding and removing edges
+#G.add_edge(5,100)
+#G.remove_edge(5,100)
+#G.add_edges_from()
+#G.remove_edges_from()
+#G.add_weighted_edges_from([(0,1,3.0), (1,4, 7.5)])
+#G.add_path([1,2,3])
+
+#nx.draw(G, with_labels=True)
+#plt.draw()
+#plt.show()
+
+df = pd.read_csv("TimePerceptionV6.csv")
+df.head()
+TimePerception = nx.from_pandas_edgelist(df, source="Sex (1=male, 2=female)", target="Delta")
+type(TimePerception)
+#nx.info(us_graph)
+TimePerception.nodes()
+len(TimePerception.nodes())
+TimePerception.edges()
+len(TimePerception.edges())
+
+#TimePerception.add_edge("Franklin", "Lincoln")
+TimePerception.nodes()
+
+nx.draw(TimePerception, with_labels=True, node_size=750, edge_color='aqua',
+        node_color='Beige', font_size=8, font_color='black')
+plt.draw()
+plt.show()
