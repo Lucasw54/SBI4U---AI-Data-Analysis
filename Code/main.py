16d04db40a756c5d40b9a92ae973735f86c80964
Lucasw54 <100497443+Lucasw54@users.noreply.github.com>
2023-06-13 02:07:50 -0400

    Clean Up + NODE DIAGRAMS

--- Code/main.py
+++ Code/main.py
@@ -9,6 +9,8 @@ import plotly.graph_objects as go
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+from collections import Counter
+import matplotlib.cm as cm
 
 
 dataset = pd.read_csv('TimePerceptionV6.csv')
@@ -29,7 +31,7 @@ model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-history = model.fit(x_train, y_train, epochs=500)
+history = model.fit(x_train, y_train, epochs=100)
 
 model.evaluate(x_test, y_test)
 
@@ -38,21 +40,17 @@ history.history.keys()
 z = 'accuracy'
 print(z)
 
-#plt.plot(history.history['accuracy'])
-#plt.plot(history.history['loss'])
-#plt.title('model accuracy/loss')
-#plt.ylabel('loss | accuracy')
-#plt.xlabel('epoch')
-#plt.legend(['accuracy', 'loss'], loc='upper left')
-#plt.show()
+
+
 
 df = pd.read_csv('TimePerceptionV6.csv')
 
 fig = px.bar(df, x='Sleep (h)', y='Delta', title='Time Perception Madness')
 #fig.show()
 
-fig2 = px.bar(df, x='Sleep (h)', y='Delta', color='Age',title='Time Perception Madness')
+fig2 = px.bar(df, x='Sleep (h)', y='Delta', color='Sex',title='Time Perception Madness')
 fig2.show()
+
 
 df = pd.read_csv('TimePerceptionV6.csv', delimiter=',', encoding='utf-8')
 fig3 = go.Figure()
@@ -66,42 +64,28 @@ fig3.update_layout(title='Time Perception Madness')
 fig3.update_layout(xaxis_title='Sleep (h)', yaxis_title='Delta')
 fig3.show()
 
-#G = nx.Graph()
-
-#Add nodes
-#G.add_node('Berea College')
-#G.add_nodes_from(range(1,10))
-
-#Remove nodes
-#G.remove_node('Berea College')
-#G.remove_nodes_from([(1,2), (2,3)])
-
-#adding and removing edges
-#G.add_edge(5,100)
-#G.remove_edge(5,100)
-#G.add_edges_from()
-#G.remove_edges_from()
-#G.add_weighted_edges_from([(0,1,3.0), (1,4, 7.5)])
-#G.add_path([1,2,3])
-
-#nx.draw(G, with_labels=True)
-#plt.draw()
-#plt.show()
-
+# ------------- Node Diagrams ------------
 df = pd.read_csv("TimePerceptionV6.csv")
 df.head()
-TimePerception = nx.from_pandas_edgelist(df, source="Sex (1=male, 2=female)", target="Delta")
+TimePerception = nx.from_pandas_edgelist(df, source="Sleep (h)", target="Age")
 type(TimePerception)
-#nx.info(us_graph)
+
+# Calculate node frequencies based on occurrence in the DataFrame
+node_frequencies = pd.concat([df["Sleep (h)"], df["Age"]]).value_counts().to_dict()
+node_frequencies2 = pd.concat([df["Sleep (h)"], df["Age"]]).value_counts().to_dict()
+
 TimePerception.nodes()
 len(TimePerception.nodes())
 TimePerception.edges()
 len(TimePerception.edges())
-
-#TimePerception.add_edge("Franklin", "Lincoln")
 TimePerception.nodes()
 
-nx.draw(TimePerception, with_labels=True, node_size=750, edge_color='aqua',
-        node_color='Beige', font_size=8, font_color='black')
+
+# Calculate node sizes based on frequencies
+node_sizes = [node_frequencies.get(node, 1) * 250 for node in TimePerception.nodes()]
+edge_width = [node_frequencies.get(node, 1) * 0.25 for node in TimePerception.nodes()]
+
+nx.draw(TimePerception, width = edge_width, with_labels=True, node_size=node_sizes, edge_color = 'red', edgecolors='grey',
+        node_color='beige', font_size=8, font_color='black')
 plt.draw()
 plt.show()
